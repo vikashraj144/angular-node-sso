6abeaed1eb68714975377b3574ff038452c3a752
vikash <vikashraj144@gmail.com>
2020-02-20 12:30:45 +0530

    auth componet

---
+++ src/app/core/helper/auth.guard.ts
@@ -0,0 +1,28 @@
+﻿import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { AuthenticationService } from '../services';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const currentUser = this.authenticationService.currentUserValue;
+        console.log("TCL: AuthGuard -> canActivate -> currentUser", currentUser);
+        console.log('====================================');
+        console.log(localStorage.getItem('token'));
+        console.log('====================================');
+        if (localStorage.getItem('token')) {
+            // authorised so return true
+            return true;
+        }
+
+        // not logged in so redirect to login page with the return url
+        this.router.navigate(['/']);
+        return false;
+    }
+}
